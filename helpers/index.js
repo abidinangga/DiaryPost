23f5161a6f68070e6b729a0f02946261f202c775
unknown <abidinangga89@gmail.com>
2022-03-25 01:02:22 +0700

    datafull

--- helpers/index.js
+++ helpers/index.js
@@ -5,25 +5,4 @@ function date(data) {
     return newDate;
 }
 
-// const transporter = nodemailer.createTransport({
-//   service: "hotmail",
-//   auth: {
-//     user: "dairypost@outlook.com",
-//     pass: "Berandal89",
-//   },
-// });
-
-// let option = {
-//   from: "dairypost@outlook.com",
-//   to: "ignasiusberneo95@gmail.com",
-//   subject: "Registration Sucess!",
-//   text: `Welcome to the jungle`,
-// };
-// transporter.sendMail(option, (err, info) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-// }
-// });
-
 module.exports = { date };
